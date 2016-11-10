e848ad8b76bb3d02df4c700bffdff2cb0d782789
zaturnvalley <zacharycmason@gmail.com>
2016-11-09 16:49:31 -0800

    need to fix bug re: ownerDocument of null

--- pie.js
+++ pie.js
@@ -8,20 +8,30 @@
           });
   var manually = function (data, x, y) {
     var colors = d3.scale.category20c();
-    total = d3.sum(data.map(function (d) { return d.value; })),
-    offset = function (d, i) {
-      return d3.sum(data.slice(0, i).map(function(d){
-        return 2*Math.PI*(d.value/total);
-      }));
-    },
-    arc = d3.svg.arc()
-          .outerRadius(150)
-          .startAngle(offset)
-          .endAngle(function (d, i) {
-            return offset(d, i)+2*Math.PI*(d.value/total);
-          }),
-  }
-  d3.json('ufo-types.json', function(data){
+        total = d3.sum(data.map(function (d) { return d.value; })),
+        offset = function (d, i) {
+          return d3.sum(data.slice(0, i).map(function(d){
+            return 2*Math.PI*(d.value/total);
+          }));
+        },
+        arc = d3.svg.arc()
+              .outerRadius(150)
+              .startAngle(offset)
+              .endAngle(function (d, i) {
+                return offset(d, i)+2*Math.PI*(d.value/total);
+              }),
+        slice = svg.selectAll('.slice')
+              .data(data)
+              .enter()
+              .append('g')
+              .attr('transform', "translate('+x+', '+y+')");
+    slice.append('path')
+        .attr({d: arc,
+              fill: function (d, i) { return colors(i); },
+              title: function (d) { return d.label + ' ('+d.value+')'; }}); 
+  };
+
+  d3.json('ufo-kind.json', function(data){
     manually(data, 300, 250);
   });
 })();
